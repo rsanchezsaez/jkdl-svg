b717a84d66208a02ff0442edaa80b93e1e285359
Arnaud Bellec <arnaud.bellec@gmail.com>
2011-07-03 22:19:42 +0200

    Switched to so version of boost

--- tests/geometry.cxx
+++ tests/geometry.cxx
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE Geometry
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
 #include "vertex.h"
 #include "network.h"
--- tests/loader.cxx
+++ tests/loader.cxx
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE Loader
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
 #include "loader.h"
 #include "network.h"
